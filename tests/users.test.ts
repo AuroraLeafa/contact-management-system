053e2a56f5a413b112b943c91d2578b6faf00da4
Refansyach <revansahdr@gmail.com>
2025-01-04 17:28:25 +0700

    Add: Login for users API

--- tests/users.test.ts
+++ tests/users.test.ts
@@ -4,9 +4,9 @@ import supertest from "supertest";
 import { UsersUtil } from "./users-util";
 
 describe("POST /api/users", () => {
-  afterEach( async () => {
+  afterEach(async () => {
     await UsersUtil.delete();
-  })
+  });
 
   it("should create a new user", async () => {
     const response = await supertest(web).post("/api/users").send({
@@ -33,4 +33,26 @@ describe("POST /api/users", () => {
   });
 
   // it("should reject new user if user already exists", () => {});
+});
+
+describe("LOGIN /api/users/login", () => {
+  beforeEach(async () => {
+    await UsersUtil.create();
+  });
+
+  afterEach(async () => {
+    await UsersUtil.delete();
+  });
+
+  it("should login user", async () => {
+    const response = await supertest(web).post("/api/users/login").send({
+      username: "TestUser",
+      password: "TestUser",
+    });
+    logger.debug(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body.data.username).toBe("TestUser");
+    expect(response.body.data.name).toBe("TestUser");
+    expect(response.body.data.token).toBeDefined;
+  });
 });
